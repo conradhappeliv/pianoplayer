34de3540b0cdfad0a7e080cac7307960fcc5dd75
Conrad H. Appel, IV <conrad.h.appel.iv@gmail.com>
2015-01-03 17:25:13 -0600

    more bug fixes
    pip should only download/install once

--- install_reqs.sh
+++ install_reqs.sh
@@ -3,11 +3,17 @@
 # midicsv
 mkdir tmp
 curl http://www.fourmilab.ch/webtools/midicsv/midicsv-1.1.tar.gz | tar xvz -C ./tmp
-cd tmp && make && make install
-rm -R tmp
+cd ./tmp/midicsv-1.1
+make && make install
+cd ../..
+rm -Rf tmp
 
 # pip
-curl https://bootstrap.pypa.io/get-pip.py | python
+if pip; then
+    echo "pip already installed"
+else
+    curl https://bootstrap.pypa.io/get-pip.py | python
+fi
 
 # pyserial
 pip install pyserial
