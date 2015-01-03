1b287d0c89d395e7d8416b39d3d12a0d7e0e7884
Conrad H. Appel, IV <conrad.h.appel.iv@gmail.com>
2015-01-03 17:20:31 -0600

    switched from wget to curl
    hopefully edison supports it this time

--- install_reqs.sh
+++ install_reqs.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 # midicsv
-wget -q0- http://www.fourmilab.ch/webtools/midicsv/midicsv-1.1.tar.gz | tar xvz -C ./tmp
+mkdir tmp
+curl http://www.fourmilab.ch/webtools/midicsv/midicsv-1.1.tar.gz | tar xvz -C ./tmp
 cd tmp && make && make install
 rm -R tmp
 
 # pip
-wget -q0- https://bootstrap.pypa.io/get-pip.py | python
+curl https://bootstrap.pypa.io/get-pip.py | python
 
 # pyserial
 pip install pyserial
