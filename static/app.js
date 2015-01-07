d011567c69d3aa38e45c7b90d0f232635f392522
Conrad H. Appel, IV <conrad.h.appel.iv@gmail.com>
2015-01-06 19:50:04 -0600

    adds list feature to webpage

--- static/app.js
+++ static/app.js
@@ -1,3 +1,7 @@
+var playsong = function(element) {
+    $.post('start', {"filename": element.innerHTML});
+}
+
 $('#start').click(function() {
     $.post('start');
 });
@@ -5,3 +9,11 @@ $('#start').click(function() {
 $('#stop').click(function() {
     $.post('stop');
 });
+
+$(document).ready(function() {
+    $.get('getlist', function(data) {
+        for(var i = 0; i < data.length; i++) {
+            $('#list').prepend('<div class="row"><a href="#" onclick="playsong(this)">'+ data[i] +'</a></div>');
+        }
+    });
+})
