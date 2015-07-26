62521afb5cf0a2f6403699edc86991c0e410ac2a
Conrad H. Appel, IV <me@conradhappeliv.com>
2015-07-26 12:11:20 -0500

    Refactored to send direct MIDI bytes, 5 second "buffer" introduced, client keyboard piano

--- client.py
+++ client.py
@@ -1,24 +1,56 @@
 import zmq
 import settings
+import queue
+import threading
+import curses
+import time
 import mido
 
-from translators import generate_command
+last_time = time.time()
+stdscr = curses.initscr()
+stdscr.keypad(1)
+stdscr.refresh()
 
-zmq_context = zmq.Context()
+q = queue.Queue()
 
-def main():
+
+def player():
+    global last_time
+    while True:
+        key = stdscr.getch()
+        now_time = time.time()
+        timing = now_time - last_time
+        timing = int(timing*1000)
+        q.put((chr(key), timing))
+        last_time = now_time
+
+
+def sender():
+    zmq_context = zmq.Context()
     zmq_socket = zmq_context.socket(zmq.REQ)
     zmq_socket.connect('tcp://127.0.0.1:'+str(settings.PORT))
+    while True:
+        letter, timing = q.get()
+        if letter == 'a':
+            note = 60
+        elif letter == 's':
+            note = 62
+        elif letter == 'd':
+            note = 64
+        elif letter == 'f':
+            note = 65
+        elif letter == 'g':
+            note = 67
+        message = mido.Message('note_on', note=note, velocity=90)
+        message_bytes = timing.to_bytes(4, 'big')  # allow 4 bytes for timing (up to 4294967.296 seconds)
+        message_bytes += message.bin()
+        zmq_socket.send(message_bytes)
+        zmq_socket.recv_string()
 
-    inport = mido.open_input(settings.INPUT_PORT)
 
-    for msg in inport:
-        print(msg)
-        # sustain is control 64
-        comm = generate_command(msg.type, 1000, msg.note, msg.velocity)
-        zmq_socket.send(comm)
-        zmq_socket.recv_string()  # ack'd
-
+def main():
+    threading.Thread(target=player).start()
+    threading.Thread(target=sender).start()
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -5,41 +5,41 @@ import queue
 import time
 import mido
 
-from translators import decode_command
-
 q = queue.Queue()
+
 
 def server():
     zmq_context = zmq.Context()
     zmq_socket = zmq_context.socket(zmq.REP)
-    zmq_socket.bind('tcp://127.0.0.1:'+str(settings.PORT))
+    zmq_socket.bind('tcp://0.0.0.0:'+str(settings.PORT))
 
     while True:
-        q.put(zmq_socket.recv())
+        q.put((zmq_socket.recv(), time.time()))
         zmq_socket.send_string('ack')  # pattern requires reply
+
 
 def player():
     last_played_at = time.time()
     port = settings.PORT_TYPE()
 
     while True:
-        cur = q.get()  # blocks until something in queue
-        decoded = decode_command(cur)
-        diff = last_played_at + decoded['timing']/1000 - time.time()
-        # TODO: add "wake-up" for piano
-        if diff > 0:
+        cur, recv_time = q.get()  # blocks until something in queue
+        timing = int.from_bytes(cur[0:4], 'big')
+
+        while time.time() - recv_time < 5: pass # 5 second "buffer" time
+
+        diff = last_played_at + timing/1000 - time.time()
+
+        if time.time() - last_played_at > 30: # piano wakeup
+            port.send(mido.Message('reset'))
+            time.sleep(2.5)
+
+        if diff > 0:  # timing between notes
             time.sleep(diff)
-        if decoded['command'] == settings.MAPPINGS.RESET:
-            msg = mido.Message('reset')
-        elif decoded['command'] == settings.MAPPINGS.NOTE_ON:
-            msg = mido.Message('note_on', note=decoded['data'][0], velocity=decoded['data'][1])
-        elif decoded['command'] == settings.MAPPINGS.NOTE_OFF:
-            msg = mido.Message('note_off', note=decoded['data'][0])
-        elif decoded['command'] == settings.MAPPINGS.SUS_ON:
-            msg = mido.Message('control')  # TODO
-        elif decoded['command'] == settings.MAPPINGS.SUS_OFF:
-            msg = mido.Message('control')  # TODO
-        port.send(msg)
+
+        message = mido.parse(cur[4:])
+
+        port.send(message)
         last_played_at = time.time()
 
 def main():
